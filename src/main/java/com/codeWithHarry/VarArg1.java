c681f940ac94c7f68f3f55fe4951ce8dd1e39bbd
“prakshh” <prakashdas18@gmail.com>
2021-10-05 10:38:25 +0530

    VarArgs1 extra param

--- src/main/java/com/codeWithHarry/VarArg1.java
+++ src/main/java/com/codeWithHarry/VarArg1.java
@@ -1,7 +1,16 @@
 package com.codeWithHarry;
 
 public class VarArg1 {
+
     static int sum(int ...arr){
+        int res = 0;
+        for(int a : arr){
+            res += a;
+        }
+        return res;
+    }
+
+    static int sum2(int x, int ...arr){
         int res = 0;
         for(int a : arr){
             res += a;
@@ -11,5 +20,6 @@ public class VarArg1 {
 
     public static void main(String[] args) {
         System.out.println("sum of 3,4,5 = " +sum(3,4,5));
+        System.out.println("sum of 3,4,5 = " +sum2(3,4,5));
     }
 }
